e7062822d18672e1286d9601b35dc7307aaced56
Ludwig N. Vasconcelos <ludwignunes@gmail.com>
2017-12-13 22:21:45 -0800

    Added new vertex

--- src/com/pythagorean/tree/App.java
+++ src/com/pythagorean/tree/App.java
@@ -3,13 +3,15 @@ package com.pythagorean.tree;
 import java.math.BigDecimal;
 
 public class App {
+	static final BigDecimal ANGLE = new  BigDecimal(0);
+	
 	public static void main(String[] args) {
 		final BigDecimal first = new BigDecimal(5);
 		final BigDecimal second = new BigDecimal(4);
 		final BigDecimal third = new BigDecimal(3);
 		
 		
-		new Square(new Point(0, 0), 5, 1, )
+		new Square(new Point(0, 0), new BigDecimal(5), 1);
 		
 		
 	}
--- src/com/pythagorean/tree/Square.java
+++ src/com/pythagorean/tree/Square.java
@@ -3,7 +3,6 @@ package com.pythagorean.tree;
 import java.math.BigDecimal;
 
 public class Square {
-	private final BigDecimal angle;
 
 	private final int depth;
 	private final BigDecimal side;
@@ -11,20 +10,28 @@ public class Square {
 	private final Point v2;
 	private final Point v3;
 	private final Point v4;
-
+	private Square left;
+	private Square right;
+	
 	// private final Square previous;
-	public Square(BigDecimal side, Point startingPoint ,BigDecimal angle) {
+	public Square(Point startingPoint, BigDecimal side, int depth) {
+		this.depth = depth;
 		this.side = side;
-		v1 = 
-		myPoints(angle);
-		// TODO Auto-generated constructor stub
+		v1 = getV1(angle);
+		v2 = getV2(angle);
+		v3 = getV3(angle);
+		v4 = getV4(angle);
 	}
 	
-	private void myPoints(BigDecimal angle2){
+	private Point getV1(BigDecimal angle2){
+		if (depth == 1 )
 		//cosAngle
 		//sinAngle
 //		v1 = new Point(,);
-		
 	}
+	
+	private Point getV2(BigDecimal angle2){}
+	private Point getV3(BigDecimal angle2){}
+	private Point getV4(BigDecimal angle2){}
 	
 }
